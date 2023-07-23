ebfc907d8e61e04b80c073dfcc04593d53a84068
hdeekshith <deekshithhegde36@gmail.com>
2023-07-24 01:10:51 +0530

    fix: API input request validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { envConfig } from './config/env';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(envConfig.port);
   console.log(`App listening on port : ${envConfig.port}`);
 }
